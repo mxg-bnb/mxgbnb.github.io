40429bcaa7ed168f29f4e3a1a2b1257fd0cd6876
墨西哥华人民宿 <51971409+mxgbnb@users.noreply.github.com>
2019-06-29 14:00:58 -0500

    Create contact.cmd

---
+++ contact.cmd
@@ -0,0 +1,15 @@
+
+
+Wechat:mxgbnb
+Email:mxgbnb@gmail.com
+fb主页：https://www.facebook.com/mxgbnb
+
+![image](https://i.imgur.com/hjixYjH.jpg)
+
+网址：
+http://sina.lt/gk9u
+
+
+
+Del Valle Norte Google地图：
+https://www.google.com.hk/maps/place/Col+del+Valle+Nte,+墨西哥城聯邦區/@19.3955025,-99.171318,16z/data=!3m1!4b1!4m5!3m4!1s0x85d1ff0d45109337:0xab2dd600a452e342!8m2!3d19.3935143!4d-99.1649891
